527eb4ab589fab5743f2de36581e366aa13d3e97
habeeb <habeebalao101@gmail.com>
2022-04-27 16:31:28 +0100

    accessor methods

--- java/src/ie/tudublin/Nematode.java
+++ java/src/ie/tudublin/Nematode.java
@@ -10,7 +10,55 @@ public class Nematode {
     private String gender;
     private int eyes;
 
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+
+
+    public int getLimbs() {
+        return limbs;
+    }
+
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
     
+
+    public int getEyes() {
+        return eyes;
+    }
+
+    public void setEye(int eyes) {
+        this.eyes = eyes;
+    }
+
     
     
 }
