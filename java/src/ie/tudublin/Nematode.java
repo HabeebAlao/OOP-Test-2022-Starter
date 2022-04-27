a8ee29a9ba7cb18400193f219baa9a1c88f873a3
habeeb <habeebalao101@gmail.com>
2022-04-27 16:22:13 +0100

    new class

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,16 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Nematode {
+
+    private String name;
+    private int length;
+    private int limbs;
+    private String gender;
+    private int eyes;
+
+    
+    
+    
+}
